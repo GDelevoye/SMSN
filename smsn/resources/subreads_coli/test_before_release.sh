d10d8b7959ccaf0951c34dde771f3094c0788536
delevoye <delevoye.guillaume@gmail.com>
2021-03-31 19:24:50 +0200

    modified test

--- smsn/resources/subreads_coli/test_before_release.sh
+++ smsn/resources/subreads_coli/test_before_release.sh
@@ -2,8 +2,8 @@
 #conda activate smsn # --> We must be in the conda env
 echo "Informal test that must be ran manually before releasing"
 echo "Ensure that you're in the smsn conda environment"
-echo "cmd1 = smsn --bam ./HTVEG_Coli.subreads.bam --reference ./../e_coli_O157.fasta --output_csv ./methylationout_HTVEG_Coli.csv --verbosity INFO --add_context True --progress_bar True --nb_proc 12 --min_subreads 50 --idQvs True --CCS ./CCS_HTVEG.bam --min_identity 0.99"
-smsn --bam ./HTVEG_Coli.subreads.bam --reference ./../e_coli_O157.fasta --output_csv ./methylationout_HTVEG_Coli.csv --verbosity INFO --add_context True --progress_bar True --nb_proc 12 --min_subreads 50 --idQvs True --CCS ./CCS_HTVEG.bam --min_identity 0.99
+echo "cmd1 = smsn --bam ./HTVEG_Coli.subreads.bam --reference ./../e_coli_O157.fasta --output_csv ./methylationout_HTVEG_Coli.csv --verbosity INFO --add_context True --progress_bar True --nb_proc 12 --min_subreads 50 --idQvs True --CCS ./CCS_HTVEG.bam --min_identity 0.99 --model SP2-C2"
+smsn --bam ./HTVEG_Coli.subreads.bam --reference ./../e_coli_O157.fasta --output_csv ./methylationout_HTVEG_Coli.csv --verbosity INFO --add_context True --progress_bar True --nb_proc 12 --min_subreads 50 --idQvs True --CCS ./CCS_HTVEG.bam --min_identity 0.99 --model SP2-C2
 echo "cmd2 = smsn --bam ./HT2_Coli.subreads.bam --reference ./../e_coli_O157.fasta --output_csv ./methylationout_HT2_Coli.csv --verbosity DEBUG --add_context False --progress_bar False --nb_proc 1 --min_subreads 20 --idQvs True --model SP2-C2"
 smsn --bam ./HT2_Coli.subreads.bam --reference ./../e_coli_O157.fasta --output_csv ./methylationout_HT2_Coli.csv --verbosity DEBUG --add_context False --progress_bar False --nb_proc 1 --min_subreads 20 --idQvs True --model SP2-C2
 echo "cmd3 = smsn --bam ./HT6_Coli.subreads.bam --reference ./../e_coli_O157.fasta --output_csv ./methylationout_HT6_Coli.csv --verbosity WARNING --add_context True --progress_bar True --nb_proc 15 --min_subreads 20 --idQvs False --model SP2-C2"
